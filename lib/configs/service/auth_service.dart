1b1ae489c039b98cf4880a57affee64ff1751b88
angelSantori <antonyo.da.mm@gmail.com>
2025-07-22 12:04:43 -0700

    X

--- lib/configs/service/auth_service.dart
+++ lib/configs/service/auth_service.dart
@@ -6,12 +6,13 @@ import 'package:jmas_movil_lecturas/configs/controllers/users_controller.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
-  //final String apiURL = 'http://200.200.200.155:5000/api';
+  //  Casa
   //final String apiURL = 'https://192.168.0.2:5001/api';
-  //final String apiURL = 'http://192.168.0.2:5000/api'; //  Casa
-  //final String apiURL = 'https://200.200.200.176:5001/api';
+  final String apiURL = 'https://192.168.0.2:5001/api'; //  Casa
+  
+  //  Trabajo
   //final String apiURL = 'http://192.168.137.1:5000/api'; //  Wifi servidor
-  final String apiURL = 'https://192.168.137.1:5001/api'; //  Wifi servidor
+  //final String apiURL = 'https://192.168.137.1:5001/api'; //  Wifi servidor
 
   Users? _currentUser;
 
